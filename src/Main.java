60ba7e2e57435c3026d230c6597c16eb5d875104
Packman5045 <packman.lp@gmail.com>
2023-02-11 22:47:41 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int WRIST_BAND_AGE = 21;
+        int age = 16;
+
+        if (age >= 21)
+        {
+            System.out.println("You may have a wrist band!");
+        }
+        else
+        {
+            System.out.println("You may NOT have a wrist band.");
+        }
+
+
+
+    }
+}
